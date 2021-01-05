d344e343c078aafd0e618e3c848884a7e180d784
dgz0515 <dgz_3180103554@163.com>
2021-01-05 21:45:03 +0800

    注释，最终更改

--- src/components/tree/tree.vue
+++ src/components/tree/tree.vue
@@ -4,7 +4,7 @@
 <script>
 import * as d3 from 'd3';
 import * as d3h from 'd3-hierarchy';
-import originData from './flare-2.json';
+import originData from '../../assets/flare-2.json';
 
 export default {
   name: 'Tree',
@@ -17,9 +17,7 @@ export default {
       chart: null,
       tooltip: null,
       click: null,
-      panSpeed: 30,
       duration: 750,
-      expandAll: false,
       title: null,
       titleText: 'Tree',
       titleRectWidth: 500,
@@ -34,11 +32,10 @@ export default {
       },
       tooltipPadding : {top: 5, right: 5, bottom: 5, left: 5},
       treeLayout: { width: 25, height: 300, direction: 'left' },
-      width: 460,
+      width: 400,
       height: 460,
-      chartPadding: { top: 80, right: 80, bottom: 80, left: 80 },
-      margin: { top: 10, right: 120, bottom: 10, left: 40 },
-      data: {}
+      // data: {},
+      treeRoot: null,
     };
   },
   mounted() {
@@ -98,7 +95,7 @@ export default {
             this.title
               .select('text')
               .attr('text-anchor', 'end')
-              .attr('x', 690);
+              .attr('x', this.titleRectWidth-10);
             break;
           default:
             break;
@@ -215,12 +212,8 @@ export default {
   },
   methods: {
     initTree() {
-      // 选择svg容器
-      d3.select('#tree-container')
-        .style('width', '100rem')
-        .style('height', '100rem');
-
       // 添加base svg
+      // 添加zoom事件回调函数：变换zoomsvg
       this.svg = d3
         .select('#tree-container')
         .append('svg')
@@ -233,27 +226,26 @@ export default {
             this.zoomsvg.attr('transform', d3.event.transform);
           })
         );
+      // 添加zoomsvg，用于zoom变换
       this.zoomsvg = this.svg.append('g');
+      // 添加g
       this.g = this.zoomsvg
         .append('g')
         .attr('transform', `translate(${this.width / 2},${this.height})`);
-        // .on('click',()=>{
-        //   console.log('svg');
-        // })
-        // ;
-      // 添加links svg
+      // 添加links
       this.gLink = this.g
         .append('g')
         .attr('fill', 'none')
         .attr('stroke', this.gLinkStroke.color)
         .attr('stroke-opacity', this.gLinkStroke.opacity)
         .attr('stroke-width', this.gLinkStroke.width);
-      // 添加nodes svg
+      // 添加nodes
       this.gNode = this.g
         .append('g')
         .attr('cursor', 'pointer')
         .attr('pointer-events', 'all');
-      // load data
+      // 加载数据
+      // 处理数据得到树根treeRoot
       this.treeRoot = d3h.hierarchy(originData);
       this.treeRoot.x0 = this.treeLayout.width / 2;
       this.treeRoot.y0 = 0;
@@ -265,7 +257,6 @@ export default {
       // 初始化tree
       this.tree = d3.tree();
       this.tree.nodeSize([this.treeLayout.width, this.treeLayout.height]);
-      // console.log('init');
       this.tree(this.treeRoot);
       this.updateTree(this.treeRoot);
       // 添加图表标题
@@ -291,10 +282,12 @@ export default {
         .attr('y', this.titleRectHeight-5)
         .attr('text-anchor', 'middle')
         .attr('fill', '#000');
+      // 添加tooltip
       this.tooltip = this.g
         .append('g')
         .attr('class', 'tooltip')
         .attr('opacity', 0);
+      // 添加矩形框
       this.tooltip.append('rect')
         .attr('fill', '#eeeeee')
         .attr('width', 150)
@@ -303,39 +296,26 @@ export default {
         .attr('ry', 0)
         .attr('stroke', 'black')
         .attr('style', 'stroke-width:1');
+      // 添加文本
       this.tooltip.append('text')
         .attr('font-size',12)
         .attr('font-color', '#000')
         .attr('transform', `translate(${this.tooltipPadding.left},${this.tooltipPadding.top+12})`);
     },
-    /**
-     * @description: update tree layout when needed
-     * @param {*} source
-     * @return {*}
-     */
     updateTree(source) {
-      // console.log('update');
-      this.linkH = d3
-        .linkHorizontal()
-        .x((d) => d.y)
-        .y((d) => d.x);
-      this.g
-        .attr('writing-mode', 'horizontal-tb');
-
+      // 水平连线
+      this.linkH = d3.linkHorizontal().x((d) => d.y).y((d) => d.x);
+      this.g.attr('writing-mode', 'horizontal-tb');
       const nodes = this.treeRoot.descendants().reverse();
       const links = this.treeRoot.links();
       // Compute the new tree layout.
       this.tree(this.treeRoot);
       // transition setting
-      const transition = this.g
-        .transition().duration(this.duration);
+      const transition = this.g.transition().duration(this.duration);
       // Update the nodes…
       const node = this.gNode.selectAll('g').data(nodes, (d) => d.id);
-
       // Enter any new nodes at the parent's previous position.
-      const nodeEnter = node
-        .enter()
-        .append('g')
+      const nodeEnter = node.enter().append('g')
         .attr('transform', `translate(${source.y0},${source.x0})`)
         .attr('fill-opacity', 0)
         .attr('stroke-opacity', 0)
@@ -344,9 +324,8 @@ export default {
           d.children = d.children ? null : d._children;
           this.updateTree(d);
         });
-
-      nodeEnter
-        .append('circle')
+      // 新增节点
+      nodeEnter.append('circle')
         .attr('class', 'node')
         .attr('r', this.node.size)
         .attr('fill', (d) =>
@@ -354,9 +333,8 @@ export default {
         )
         .attr('stroke', this.color)
         .attr('stroke-width', this.node.borderWidth);
-
-      nodeEnter
-        .append('text')
+      // 新增节点的文本
+      nodeEnter.append('text')
         .attr('x', (d) => (d._children ? -6 : 6))
         .attr('dy', '.31em')
         .attr('class', 'nodeText')
@@ -365,12 +343,12 @@ export default {
         .attr('font-size', this.text.fontSize)
         .attr('font-family', this.text.fontFamily)
         .attr('fill', this.text.fontColor)
-        .clone(true)
-        .lower()
+        .clone(true).lower()
         .attr('stroke-linejoin', 'round')
         .attr('stroke-width', 3)
         .attr('stroke', 'white');
-      
+      // 对节点的文本添加mouseover和mouseout事件
+      // 显示tooltip
       this.gNode.selectAll('text')
         .on('mouseover', function(d){
           if(! d._children){
@@ -392,52 +370,35 @@ export default {
               .attr('opacity',0);
           }
         });
-
+      // 控制节点文本的显示
       if (this.options.textIsShow) {
         this.gNode.selectAll('text').attr('style', 'display: block');
       } else {
         this.gNode.selectAll('text').attr('style', 'display: none');
       }
       // Transition nodes to their new position.
-
-      node
-        .merge(nodeEnter)
-        .transition(transition)
+      node.merge(nodeEnter).transition(transition)
         .attr('transform', (d) => `translate(${d.y},${d.x})`)
         .attr('fill-opacity', 1)
         .attr('stroke-opacity', 1);
-
       // Transition exiting nodes to the parent's new position.
-      node
-        .exit()
-        .transition(transition)
-        .remove()
+      node.exit().transition(transition).remove()
         .attr('transform', `translate(${source.y0},${source.x0})`)
         .attr('fill-opacity', 0)
         .attr('stroke-opacity', 0);
-
       // update the links...
       const link = this.gLink.selectAll('path').data(links, (d) => d.target.id);
       // Enter any new links at the parent's previous position.
-      const linkEnter = link
-        .enter()
-        .append('path')
+      const linkEnter = link.enter().append('path')
         .attr('d', () => {
           const o = { x: source.x0, y: source.y0 };
           return this.linkH({ source: o, target: o });
         });
-
       // Transition links to their new position.
-      link
-        .merge(linkEnter)
-        .transition(transition)
+      link.merge(linkEnter).transition(transition)
         .attr('d', this.linkH);
-
       // Transition exiting nodes to the parent's new position.
-      link
-        .exit()
-        .transition(transition)
-        .remove()
+      link.exit().transition(transition).remove()
         .attr('d', () => {
           const o = { x: source.x, y: source.y };
           return this.linkH({ source: o, target: o });
@@ -448,9 +409,11 @@ export default {
         d.y0 = d.y;
       });
     },
+    // 根据节点，映射一种颜色
     color(node){
       const color = d3.scaleOrdinal()
-        .domain(this.treeRoot.descendants().filter(d => d.depth<=1).map(d => d.data.name))
+        .domain(this.treeRoot.descendants()
+          .filter(d => d.depth<=1).map(d => d.data.name))
         .range(d3.schemeCategory10);
       if(node.depth===0){
         return color(node.data.name);
